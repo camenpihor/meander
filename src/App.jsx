4b4cebdfe9cbd39a3abc61d22e21e0ebed95692e
camen <camen.piho.r@gmail.com>
2024-08-18 18:40:47 -0400

    Begin adding map modes; handle popup control better

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import "./App.css";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
+const NORMAL_MODE = 0;
+const ADD_MODE = 1;
+const DELETE_MODE = 2;
+
 const fetchTreeLocations = async () => {
   const response = await fetch(`${process.env.PUBLIC_URL}/assets/tree_locations.csv`);
   const csvData = await response.text();
@@ -142,6 +146,8 @@ const App = () => {
       className: "text-medium",
     })
   );
+  const [currentPopupLocation, setCurrentPopupLocation] = useState([]);
+  const additionalMapBoxButtonsRef = useRef(null);
   const [treeLocations, setTreeLocations] = useState([]);
   const [treeInfo, setTreeInfo] = useState([]);
   const [visibleTrees, setVisibleTrees] = useState([]);
@@ -149,6 +155,17 @@ const App = () => {
   const [isTreeListVisible, setIsTreeListVisible] = useState(false);
   const [isHamburgerVisible, setIsHamburgerVisible] = useState(true);
   const [isHamburgerFadingOut, setIsHamburgerFadingOut] = useState(false);
+  const [mapMode, setMapMode] = useState(0);
+
+  const addTree = (event) => {
+    console.log("adding tree", event)
+    setMapMode(NORMAL_MODE)
+  }
+
+  const removeTree = (event) => {
+    console.log("removing tree",  event.features[0].properties);
+    setMapMode(NORMAL_MODE)
+  }
 
   const highlightTree = async (treeName) => {
     if (mapRef.current && treeName) {
@@ -208,6 +225,16 @@ const App = () => {
     }, 500)
   };
 
+  const updateMapMode = (toMode) => {
+    if (mapMode === toMode) {
+      console.log("unsetting mode");
+      setMapMode(NORMAL_MODE);
+    } else {
+      console.log("setting mode", toMode)
+      setMapMode(toMode);
+    }
+  }
+
   useEffect(() => {
     fetchTreeInfo().then(data => setTreeInfo(data));
     fetchTreeLocations().then(data => setTreeLocations(data));
@@ -230,6 +257,7 @@ const App = () => {
       showUserHeading: true,
     });
     map.addControl(geolocateControl, "top-right");
+    document.querySelector('.mapboxgl-ctrl-top-right').appendChild(additionalMapBoxButtonsRef.current);
 
     map.on("load", () => {
       geolocateControl.trigger();
@@ -237,12 +265,8 @@ const App = () => {
       map.once("idle", async () => {
         await updateVisibleTrees(map, setVisibleTrees);
       });
-      map.on("mouseenter", "unclustered-point", () => {
-        map.getCanvas().style.cursor = "pointer";
-      });
-      map.on("mouseleave", "unclustered-point", () => {
-        map.getCanvas().style.cursor = "";
-      });
+      mapRef.current.on("mouseenter", "unclustered-point", () => {mapRef.current.getCanvas().style.cursor = "pointer"});
+      mapRef.current.on("mouseleave", "unclustered-point", () => {mapRef.current.getCanvas().style.cursor = ""});
       map.on("touchstart", () => {
         setIsTreeListVisible(false);
         setIsHamburgerVisible(true);
@@ -262,6 +286,7 @@ const App = () => {
       const tree = treeInfo[locationProperties.tree_id]
       const popupContent = createPopupContent(tree, locationProperties);
       setTimeout(() => {
+        setCurrentPopupLocation(locationProperties.location_id);
         popupRef.current
           .setLngLat(coordinates)
           .setHTML(popupContent)
@@ -270,14 +295,29 @@ const App = () => {
     };
 
     const removeTreePopup = () => {
+      setCurrentPopupLocation(null);
       popupRef.current.remove();
     };
 
-    mapRef.current.on("mouseenter", "unclustered-point", createTreePopup);
+    const handlePointMovement = (event) => {
+      const newLocation = event.features[0].properties.location_id;
+      if (currentPopupLocation !== newLocation) {
+        createTreePopup(event);
+      }
+    };
+
+    mapRef.current.on("mousemove", "unclustered-point", handlePointMovement);
     mapRef.current.on("mouseleave", "unclustered-point", removeTreePopup);
     mapRef.current.on("touchstart", "unclustered-point", createTreePopup);
     mapRef.current.on("touchend", "unclustered-point", removeTreePopup);
-  }, [treeInfo])
+
+    return () => {
+      mapRef.current.off("mousemove", "unclustered-point", handlePointMovement);
+      mapRef.current.off("mouseleave", "unclustered-point", removeTreePopup);
+      mapRef.current.off("touchstart", "unclustered-point", createTreePopup);
+      mapRef.current.off("touchend", "unclustered-point", removeTreePopup);
+    }
+  }, [treeInfo, currentPopupLocation])
 
   useEffect(() => {
     const debouncedUpdateMap = debounce(() => updateMap(highlightedTree), 300);
@@ -293,9 +333,46 @@ const App = () => {
     };
   }, [highlightedTree, updateMap]);
 
+  useEffect(() => {
+    if (mapRef.current) {
+      if (mapMode === NORMAL_MODE) {
+        setIsTreeListVisible(true);
+      } else if (mapMode === ADD_MODE) {
+        setIsTreeListVisible(false);
+        mapRef.current.on("click", addTree);
+        mapRef.current.on("touchend", addTree);
+      } else if (mapMode === DELETE_MODE) {
+        setIsTreeListVisible(false);
+        mapRef.current.on("click", "unclustered-point", removeTree);
+        mapRef.current.on("touchend", "unclustered-point", removeTree);
+      }
+    }
+    return () => {
+      if (mapRef.current) {
+        mapRef.current.off("click", "unclustered-point", removeTree);
+        mapRef.current.off("touchend", "unclustered-point", removeTree);
+        mapRef.current.off("click", addTree);
+        mapRef.current.off("touchend", addTree);
+      }
+    };
+  }, [mapMode]);
+
   return (
     <div>
       <div ref={mapContainerRef} className="w-screen h-screen" />
+      {mapMode !== NORMAL_MODE && (
+        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 pointer-events-none"></div>
+      )}
+      {mapMode === ADD_MODE && (
+        <div className="absolute top-4 left-1/2 transform -translate-x-1/2 text-lg text-white bg-orange-500 p-2 rounded">
+          Adding Tree...
+        </div>
+      )}
+      {mapMode === DELETE_MODE && (
+        <div className="absolute top-4 left-1/2 transform -translate-x-1/2 text-lg text-white bg-orange-500 p-2 rounded">
+          Removing Tree...
+        </div>
+      )}
       {isHamburgerVisible && (
         <button
           className={`fixed top-4 left-4 bg-white border border-gray-300 p-2 rounded md:hidden z-50 transition-opacity duration-500 ${isHamburgerFadingOut ? 'opacity-0' : 'opacity-100'}`}
@@ -306,7 +383,7 @@ const App = () => {
       )}
 
       <div
-        className={`absolute top-0 left-0 bg-white p-4 shadow-lg max-h-screen overflow-y-auto z-40 transition-transform transform duration-500 ${isTreeListVisible ? 'translate-x-0' : '-translate-x-[120%]'} md:translate-x-0 md:block`}
+        className={`absolute top-0 left-0 bg-white p-4 shadow-lg max-h-screen overflow-y-auto z-40 transition-transform transform duration-500 ${isTreeListVisible ? 'translate-x-0' : '-translate-x-[120%]'}`}
       >
         <h3 className="text-lg font-bold mb-2">Visible Trees</h3>
         <ul>
@@ -320,6 +397,16 @@ const App = () => {
             </li>
           ))}
         </ul>
+      </div>
+
+      {/* Hidden buttons to be added to the map control later */}
+      <div ref={additionalMapBoxButtonsRef} className="mapboxgl-ctrl mapboxgl-ctrl-group">
+        <button className="mapboxgl-ctrl-zoom-in" title="Add Tree" onClick={() => updateMapMode(ADD_MODE)}>
+          <span className={`mapboxgl-ctrl-icon ${mapMode === ADD_MODE ? "bg-orange-300" : ""}`} aria-hidden="true" title="Zoom in"></span>
+        </button>
+        <button className="mapboxgl-ctrl-zoom-out" title="Remove Tree" onClick={() => updateMapMode(DELETE_MODE)}>
+          <span className={`mapboxgl-ctrl-icon ${mapMode === DELETE_MODE ? "bg-orange-300" : ""}`} aria-hidden="true" title="Zoom out"></span>
+        </button>
       </div>
     </div>
   );
